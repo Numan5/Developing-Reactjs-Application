14c5ea49ad64fe344961b10f7b33dab67a03399e
Numan Güngör <numangungor1970@gmail.com>
2022-03-06 23:48:44 +0300

    number-guessing game

---
+++ src/componenets/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+        <img src="img/number.jpg"alt=""className='resim'/>
+        </div>
+    )
+}
+export default Header
